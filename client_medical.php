8de9a7a4a4d9aa2d79ea442ad125ca4e14346aef
Buyinza David <davidbuyinza256@gmail.com>
2020-01-29 04:18:52 +0300

    Fixed filter bug in client_medical

--- client_medical.php
+++ client_medical.php
@@ -2763,8 +2763,8 @@ $result = mysqli_query($mysqli, "SELECT * FROM  cmpatientsregistration WHERE SER
         }
         var dest_name = event.id;
         dest_name =  "p" + dest_name.substring(1);
-        var alt = document.getElementsByName(dest_name)[0];
-        alert(alt);
+        //var alt = document.getElementsByName(dest_name)[0];
+        alert("'" + dest_name + "'");
         var nodes = document.getElementsByName(dest_name)[0].getElementsByTagName("optgroup");
        
         
