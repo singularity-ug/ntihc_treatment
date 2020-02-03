8e81e5aa8c814060f7be4b80dcac383bcb0ac5cb
David Buyinza <davidbuyinza256@gmail.com>
2020-02-03 15:31:03 +0300

    Modified treatmentroom_process_backup.php saving tests in respective table

--- registration_desk.php
+++ registration_desk.php
@@ -539,7 +539,57 @@ else{
                     </table>
                 </div>
 
-                <div class="col-sm-6"> <?php include('reactant.js'); ?>
+                <div class="col-sm-6"> 
+
+                <script type="text/javascript">
+function showDiv(prefix,chooser) 
+{
+        for(var i=0; i<chooser.options.length; i++) 
+		{
+        	var div = document.getElementById(prefix+chooser.options[i].value);
+            div.style.display = 'none';
+        }
+ 
+		var selectedOption = (chooser.options[chooser.selectedIndex].value);
+		
+ 
+		if(selectedOption == "1")
+		{
+			displayDiv(prefix,"1");
+		}
+		else if(selectedOption == "PREGNANCY RELATED SERVICES")
+		{
+			displayDiv(prefix,"PREGNANCY RELATED SERVICES");
+		}
+		else if(selectedOption == "MEDICAL SERVICES")
+		{
+			displayDiv(prefix,"MEDICAL SERVICES");
+		}
+		else if(selectedOption == "COUNSELLING SERVICES")
+		{
+			displayDiv(prefix,"COUNSELLING SERVICES");
+		}
+		else if(selectedOption == "OUTREACH")
+		{
+			displayDiv(prefix,"OUTREACH");
+		}
+		else if(selectedOption == "OTHER SERVICES")
+		{
+			displayDiv(prefix,"OTHER SERVICES");
+		}
+ 
+}
+ 
+function displayDiv(prefix,suffix) 
+{
+        var div = document.getElementById(prefix+suffix);
+        div.style.display = 'block';
+}
+
+</script>
+
+
+<!--=========================================================================================================================-->
 
                     <table border="1" cellpadding="4" cellspacing="0" style="font-family: arial; font-size: 12px; border:0px;text-align:left; margin-top:5px;" width="100%">
                         <thead>
